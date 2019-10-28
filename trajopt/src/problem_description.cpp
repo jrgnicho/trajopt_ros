1943ab0b822e20fb31fed29853e0944b63bd5d3e
Matthew Powelson <mwpowelson42@students.tntech.edu>
2019-10-28 12:47:50 -0500

    looks like minor changes to adapt to different interace for trajopt.

--- trajopt/src/problem_description.cpp
+++ trajopt/src/problem_description.cpp
@@ -650,12 +650,12 @@ void DynamicCartPoseTermInfo::hatch(TrajOptProb& prob)
     if (term_type & TT_COST)
     {
       prob.addCost(sco::Cost::Ptr(
-          new TrajOptCostFromErrFunc(f, dfdx, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::ABS, name)));
+          new TrajOptCostFromErrFunc(f, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::ABS, name)));
     }
     else if (term_type & TT_CNT)
     {
       prob.addConstraint(sco::Constraint::Ptr(
-          new TrajOptConstraintFromErrFunc(f, dfdx, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::EQ, name)));
+          new TrajOptConstraintFromErrFunc(f, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::EQ, name)));
     }
     else
     {
@@ -794,7 +794,7 @@ void CartPoseTermInfo::hatch(TrajOptProb& prob)
     sco::MatrixOfVector::Ptr dfdx(
         new CartPoseJacCalculator(input_pose, prob.GetKin(), adjacency_map, world_to_base, link, tcp, indices));
     prob.addCost(
-        sco::Cost::Ptr(new TrajOptCostFromErrFunc(f, dfdx, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::ABS, name)));
+        sco::Cost::Ptr(new TrajOptCostFromErrFunc(f, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::ABS, name)));
   }
   else if ((term_type & TT_CNT) && ~(term_type | ~TT_USE_TIME))
   {
@@ -803,7 +803,7 @@ void CartPoseTermInfo::hatch(TrajOptProb& prob)
     sco::MatrixOfVector::Ptr dfdx(
         new CartPoseJacCalculator(input_pose, prob.GetKin(), adjacency_map, world_to_base, link, tcp, indices));
     prob.addConstraint(sco::Constraint::Ptr(
-        new TrajOptConstraintFromErrFunc(f, dfdx, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::EQ, name)));
+        new TrajOptConstraintFromErrFunc(f, prob.GetVarRow(timestep, 0, n_dof), coeff, sco::EQ, name)));
   }
   else
   {
